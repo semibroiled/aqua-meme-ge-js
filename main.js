728529ccbab30434a4e0f2c7c399a16223ede32f
semibroiled <amitavcmostafa@gmx.de>
2021-08-24 18:42:28 +0200

    Fix linking

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 //Get element via id 'image' in HTML and put source to one version of gif
 function aqua() {
-document.getElementById('Image').src='aqua.gif';
+document.getElementById('Image').src='https://raw.githubusercontent.com/semibroiled/aqua-meme-ge-js/master/aqua.gif';
 }
 //We're changing the same image
 //Get element via id 'image' in HTML and put source to other version of gif
 function alsoAqua() {
-    document.getElementById('Image').src='also-aqua.gif';
+    document.getElementById('Image').src='https://raw.githubusercontent.com/semibroiled/aqua-meme-ge-js/master/also-aqua.gif';
 }
