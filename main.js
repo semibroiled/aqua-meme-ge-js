9d2e71dd94ece0152373c74d7472b0ca6c6388f2
semibroiled <amitavcmostafa@gmx.de>
2021-08-24 18:34:52 +0200

    Add bg

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+//Get element via id 'image' in HTML and put source to one version of gif
 function aqua() {
 document.getElementById('Image').src='aqua.gif';
 }
-
+//We're changing the same image
+//Get element via id 'image' in HTML and put source to other version of gif
 function alsoAqua() {
     document.getElementById('Image').src='also-aqua.gif';
 }
